11931bfc4f417fa88bdffe583a3a3ccb9b5c9f45
vaequitas <thomas@flack.it>
2018-09-14 13:01:16 +0100

    [update_activity] Update user activity to listening

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// main bot file
+const Discord        = require('discord.js');
+const client         = new Discord.Client();
+const discord_config = require('../config/secure/discord-auth.json');
+const user_tokens    = discord_config.tokens
+
+client.on('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity('Last.fm', { type: 'LISTENING', url: 'https://github.com/vaequitas/discord-last-fm' });
+});
+
+client.login(user_tokens[0]);

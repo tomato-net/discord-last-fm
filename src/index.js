12cdb6015e9cda3b08a77872a6053865942822dc
vaequitas <thomas@flack.it>
2018-09-14 13:37:47 +0100

    [update_activity] Update song for first user to static song

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const Discord        = require('discord.js');
-const client         = new Discord.Client();
 const discord_config = require('../config/secure/discord-auth.json');
-const user_tokens    = discord_config.tokens
+const user_tokens    = discord_config.tokens;
 
-client.on('ready', () => {
-  console.log('Ready!');
-  client.user.setActivity('Last.fm', { type: 'LISTENING', url: 'https://github.com/vaequitas/discord-last-fm' });
-});
+const updateActivity = async (song, token) => {
+  const client = new Discord.Client();
+  await client.on('ready', async () => {
+    console.log('Ready!');
+    await client.user.setActivity(song, { type: 'LISTENING' });
+    client.destroy();
+  });
 
-client.login(user_tokens[0]);
+
+  client.login(token);
+};
+
+updateActivity('Amorphis', user_tokens.shift());
